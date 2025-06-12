99b75ca193818c46f664ce89e7f62d4925a6b980
Ryzh <gooddmitriy@mail.ru>
2025-06-12 10:22:25 +0300

    litexl

---
+++ lite-xl/user_settings.lua
@@ -0,0 +1,69 @@
+return {
+  ["config"] = {
+    ["borderless"] = false,
+    ["code_font"] = {
+      ["fonts"] = {
+        [1] = {
+          ["name"] = "Iosevka Regular",
+          ["path"] = "/home/ryzh/.local/share/fonts/Iosevka-Regular.ttf"
+        },
+        [2] = {
+          ["name"] = "JetBrains Mono Regular",
+          ["path"] = "/home/ryzh/Downloads/lite-xl/data/fonts/JetBrainsMono-Regular.ttf"
+        }
+      },
+      ["options"] = {
+        ["antialiasing"] = "subpixel",
+        ["bold"] = false,
+        ["hinting"] = "slight",
+        ["italic"] = false,
+        ["size"] = 22,
+        ["smoothing"] = false,
+        ["strikethrough"] = false,
+        ["underline"] = false
+      }
+    },
+    ["custom_keybindings"] = {
+      ["doc:toggle-block-comments"] = {
+        [1] = "ctrl+shift+keypad /"
+      },
+      ["doc:toggle-line-comments"] = {
+        [1] = "ctrl+keypad /"
+      },
+      ["markdown:show"] = {
+        [1] = "ctrl+shift+v"
+      },
+      ["terminal:open"] = {
+        [1] = "ctrl+`"
+      }
+    },
+    ["font"] = {
+      ["fonts"] = {
+        [1] = {
+          ["name"] = "Fira Sans Regular",
+          ["path"] = "/home/ryzh/Downloads/lite-xl/data/fonts/FiraSans-Regular.ttf"
+        },
+        [2] = {
+          ["name"] = "Ubuntu Regular",
+          ["path"] = "/usr/share/fonts/truetype/ubuntu/Ubuntu-R.ttf"
+        },
+        [3] = {
+          ["name"] = "Iosevka Regular",
+          ["path"] = "/home/ryzh/.local/share/fonts/Iosevka-Regular.ttf"
+        }
+      },
+      ["options"] = {
+        ["antialiasing"] = "subpixel",
+        ["bold"] = true,
+        ["hinting"] = "slight",
+        ["italic"] = false,
+        ["size"] = 17,
+        ["smoothing"] = false,
+        ["strikethrough"] = false,
+        ["underline"] = false
+      }
+    },
+    ["indent_size"] = 4,
+    ["theme"] = "everforest"
+  }
+}
