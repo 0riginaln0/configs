c479ec553b9b6523987cd412cffa55041fae4e72
0riginaln0 <74508026+0riginaln0@users.noreply.github.com>
2025-12-08 18:22:48 +0300

    Add files via upload

---
+++ lite-xl/user_settings.lua
@@ -0,0 +1,24 @@
+return {
+  ["config"] = {
+    ["disabled_plugins"] = {
+      ["tabnumbers"] = true
+    },
+    ["plugins"] = {
+      ["minimap"] = {
+        ["caret_color"] = {
+          [1] = 147,
+          [2] = 221,
+          [3] = 250,
+          [4] = 255
+        },
+        ["selection_color"] = {
+          [1] = 82,
+          [2] = 82,
+          [3] = 87,
+          [4] = 255
+        }
+      }
+    },
+    ["theme"] = "gruvbox_dark"
+  }
+}
